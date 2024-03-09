c52eac4df4a17c86135105017c37554f3d87eff2
David Pasumbal <djpasumbal@dj-l-sbk-mbp.lan>
2024-03-09 12:29:31 +0800

    Added base code

---
+++ src/main/java/org/example/ThreadedPrime.java
@@ -0,0 +1,21 @@
+package org.example;
+
+import java.util.*;
+
+public class ThreadedPrime {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+
+        ThreadedPrime threadedPrime = new ThreadedPrime();
+        List<Integer> primes = threadedPrime.getListOfPrimes(0,null);
+        threadedPrime.printListOfPrimes(primes);
+    }
+
+    private List<Integer> getListOfPrimes(int numberOfThreads, Integer maxNumber) {
+
+        return null;
+    }
+
+    private void printListOfPrimes(List<Integer> primes) {
+
+    }}
